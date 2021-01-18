1318d482ae5d6ebd0e8e8a1119002c47d4b9aa12
Pratham Goradia <goradiapratham@gmail.com>
2021-01-18 13:21:56 +0800

    Add input functionality for custom timer

--- App.js
+++ App.js
@@ -13,7 +13,7 @@ import {
   ScrollView,
   View,
   Text,
-  StatusBar,
+  TextInput
 } from 'react-native';
 
 import {
@@ -21,35 +21,60 @@ import {
 } from 'react-native/Libraries/NewAppScreen';
 
 const displayAsTime = (timer) => {
-    return new Date(timer * 1000).toISOString().substr(11, 8)
+    return new Date(timer * 1000).toISOString().substr(11, 8);
 }
 
 const Countdown = (props) => {
-    const [timer, setTimer] = useState(props.remainingSecond);
-    useEffect(() => {
-        const interval = setTimeout(() => {
-            if(timer === 0){
-            }
-            else{
-                setTimer(timer - 1);
-            }
-        }, 1000)
-        return () => {
-            if(timer === 1){
-                alert("Done");
-            }
-        }
-    }, [timer])
+  const [timer, setTimer] = useState(props.remainingSecond);
+  useEffect(() => {
+      const interval = setTimeout(() => {
+          if(timer === 0){
+          }
+          else{
+              setTimer(timer - 1);
+          }
+      }, 1000)
+      return () => {
+          if(timer === 1){
+              alert("Done");
+          }
+      }
+  }, [timer])
 
-    return (
-        <Text> {displayAsTime(timer)} </Text>
-    );
+  return (
+      <Text> {displayAsTime(timer)} </Text>
+  );
 };
 
 const App: () => React$Node = () => {
-  return (
-    <Countdown remainingSecond = {5} />
-  );
+  const [timerInput, setTimerInput] = useState(-1);
+   if (timerInput == -1) {
+     return(
+         <View style={{padding: 10}}>
+           <TextInput
+               placeholder="Type the timer value here!"
+               onSubmitEditing = {(event) => {
+                 if(parseInt(event.nativeEvent.text))
+                 {
+                    setTimerInput(parseInt(event.nativeEvent.text));
+                 }
+                 else
+                 {
+                    alert("Please enter a valid input");
+                 }
+               }}
+           />
+           <Text> Enter the number of seconds for the timer </Text>
+         </View>
+     );
+   }
+   else{
+     return(
+         <View style={{padding: 10}}>
+           <Countdown remainingSecond = {timerInput} />
+         </View>
+     );
+   }
 };
 
 const styles = StyleSheet.create({
@@ -66,3 +91,22 @@ const styles = StyleSheet.create({
 });
 
 export default App;
+
+/*
+    <Text>
+        {text.split(' ').map((word) => word && '🍕').join(' ')}
+    </Text>
+
+    <Text>
+        {String(text)}
+    </Text>
+
+    </>
+       /* <Countdown remainingSecond = {parseInt(text)} />*/
+
+     /*
+        <TextInput
+            placeholder="Type the timer value here!"
+            onSubmitEditing = {text => setText(text)}
+        />
+        */
